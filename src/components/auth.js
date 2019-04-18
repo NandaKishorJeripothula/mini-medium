1ea4ea474cdcfc13c70a324676352fe58c03bf05
Nandu <j.kishor.bd@gmail.com>
2019-04-18 18:02:30 +0530

    before adding login logic

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Text } from 'react-native';
 
 class Auth extends Component {
-    static propTypes = {
-        prop: PropTypes
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            usernameInputVal: '',
+            passwordInputVal: ''
+        }
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
     }
 
     render() {
         return (
-            <div>''''''''''''''''''''
-                Am Auth screen
-            </div>
+            // <Text> Hey am Atuh screen</Text>
+            if (this.state.loading === true) {
+            return (
+                <Container>
+                    <Header />
+                    <Content>
+                        <Spinner />
+                    </Content>
+                </Container>
+            );
+        } else {
+            return (
+                <Container>
+                    <Header>
+                        <Left />
+                        <Body>
+                            <Title> Login </Title>
+                        </Body>
+                        <Right />
+                    </Header>
+                    <Content contentContainerStyle={{ justifyContent: 'center', margin: 20 }}>
+                        <Form>
+                            <Item floatingLabel>
+                                <Label>Username</Label>
+                                <Input value={this.state.usernameTextBox} onChangeText={this.handleUsernameChange} />
+                            </Item>
+                            <Item floatingLabel>
+                                <Label>Password</Label>
+                                <Input value={this.state.passwordTextbox} onChangeText={this.handlePasswordChange} secureTextEntry />
+                            </Item>
+                        </Form>
+                        <View style={{ height: 10 }} />
+                        <Button block onPress={this.handleSignupPressed} >
+                            <Text> Sign up </Text>
+                        </Button>
+                        <View style={{ height: 10 }} />
+                        <Button block title="Log in" onPress={this.handleLoginPressed} >
+                            <Text> Log in </Text>
+                        </Button>
+                    </Content>
+                </Container>
+            )
+        }
         )
     }
 }
-export default connect(Auth);
+export default connect()(Auth);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,7 @@
 //The Initial State 
 const initialState = {
     //As no user logged in,its empty
-    session: { "name": "sdjhfklashdfhkl" }
+    session: {},
 }
 export default function reducer(state = initialState, action) {
     switch (action.type) {
